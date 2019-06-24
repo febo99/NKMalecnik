6b78ad6090504d51e0309e5e4d88c24311c3be1f
Žiga Lah <febo.89.89@gmail.com>
2019-06-24 16:44:47 +0200

    Delete config.php

--- login/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("SERVER", "localhost");
-define("USER", "root");
-define("PWD","rdecastrela6");
-define("DB", "NKMalecnik");
-$db = mysqli_connect(SERVER, USER, PWD, DB);
-$db->set_charset("utf8")
-?>
